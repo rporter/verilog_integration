37b6d46031a65584c0b0342144f8aa8213e83ce7
Rich Porter <dung_spreader@yahoo.com>
2013-03-21 15:46:34 +0000

    add filter slider and message index to log tab

--- www/static/report.js
+++ www/static/report.js
@@ -12,7 +12,15 @@ $report = function(){
     WARNING     : 5,
     ERROR       : 6,
     INTERNAL    : 7,
-    FATAL       : 8
+    FATAL       : 8,
+    _SIZE       : 9,
+    severity    : function (level) {
+      for (var l in this) {
+        if (this[l] === level) {
+          return l;
+	}
+      }
+    }
   };
 
   $report.report = function(){
@@ -133,10 +141,9 @@ $report = function(){
       url : 'msgs/'+event.data.log_id,
       dataType : 'json',
       success : function (json) {
+        json.forEach(function(msg){msg.date = new Date(msg.date*1000)});
         div.jqoteapp('#template', json);
-/*	json.forEach(function(msg) {
-            $('<code/>', {class:msg.severity, title:(new Date(msg.date*1000)).toGMTString(), text:ident(msg.severity)+msg.msg}).appendTo(div);
-	});*/
+        $report.openLogWidget(div);
       },
       error : function(xhr, status, index, anchor) {
         console.log(xhr, status, index);
@@ -144,6 +151,88 @@ $report = function(){
     });
     $report.tabs('add', '#'+id, event.data.log_id+' log');
   };
+
+  $report.openLogWidget = function(div) {
+    var nodes  = $('code', div);
+    var widget = $('<div class="widget"/>').appendTo(div);
+    var align  = function() {
+      widget.animate({top:$(div).scrollTop()},{duration:100,queue:false});
+    };
+    $(div).scroll(align);
+    // show/hide timestamp
+    $('<input type="checkbox">').appendTo(widget).bind('change.time', function(event){$('time', div).toggle()}).wrap('<p>Show time</p>');
+    // filter by severity
+    var slider = $('<div class="slider"/>').appendTo(widget);
+    var lower  = $("<div/>").css({ position : 'absolute' , top : 10, left : 0 }).text($report.levels.severity(0)).hide();
+    var upper  = $("<div/>").css({ position : 'absolute' , top : 10, left : 0 }).text($report.levels.severity($report.levels._SIZE-1)).hide();
+    slider.slider({
+      range: true,
+      min: 0,
+      max: $report.levels._SIZE-1,
+      values: [ 0, $report.levels._SIZE-1 ]
+    }).bind('slide.example', function(event, ui) {
+      div.hide();
+      lower.text($report.levels.severity(ui.values[0]));
+      upper.text($report.levels.severity(ui.values[1]));
+      nodes.each(function(index){if(($(this).attr('level') >= ui.values[0]) && ($(this).attr('level') <= ui.values[1])){$(this).show()}else{$(this).hide()}})
+      div.show();
+    });
+    slider.find(".ui-slider-handle:first")
+      .append(lower)
+      .bind('mouseenter.example', function(event){lower.show()})
+      .bind('mouseleave.example', function(event){lower.hide()});
+    slider.find(".ui-slider-handle:last")
+      .append(upper)
+      .bind('mouseenter.example', function(event){upper.show()})
+      .bind('mouseleave.example', function(event){upper.hide()});
+    // message indexed by severity
+    function getBySeverity() {
+      function sorted(nodes) {
+        function children(nodes) {
+          return nodes.slice(0,11).map(
+            function(it, idx){
+              return {
+                "title"    : (idx>9)?'...':$report.levels.severity(parseInt($(it).attr('level'))),
+                "key"      : it.idx
+              };
+          });
+        };
+        return Object.keys(nodes).sort().map(
+          function(it) {
+            return {
+      	      "title"    : '(' + it.slice(1) + ') ' + $report.levels.severity(parseInt(it.slice(1))) + ' <s' + parseInt(it.slice(1))/10 + '><i>' + nodes[it].length + '</i></s>',
+              "key"      : nodes[it][0].idx,
+              "children" : children(nodes[it])
+           }}
+        );
+      };
+	return sorted(nodes.toArray().reduce(function(levels, node, idx) {
+        var level = 's'+$(node).attr('level');
+        node.idx = idx;
+        if (!levels.hasOwnProperty(level)) levels[level]=[];
+        levels[level].push(node);
+        return levels;
+      }, {}));
+    }
+
+    var msgIndex = $('<div/>').appendTo(widget);
+    msgIndex.dynatree({
+      children : [
+          {title : "Messages", isFolder : true, children : getBySeverity(nodes)},
+      ],
+      onActivate: function(node) {
+          div.animate({scrollTop : $(nodes[node.data.key]).offset().top - div.offset().top + div.scrollTop() - div.height()/2});
+      },
+      onRender : function(dtnode, nodeSpan) {
+        $('a', dtnode.li).hover(
+          function(){$(nodes[$.ui.dynatree.getNode(this).data.key]).addClass(   'example-highlight')},
+          function(){$(nodes[$.ui.dynatree.getNode(this).data.key]).removeClass('example-highlight')}
+        );
+        align();
+      }
+    });
+  };
+
 
 })($report);
 
