f64cc438e783cb4a82422dc4b800ff959c24c09d
Rich Porter <dung.spreader@gmail.com>
2013-05-11 15:09:46 +0100

    split db access into separate module

---
+++ python/database.py
@@ -0,0 +1,139 @@
+# Copyright (c) 2013 Rich Porter - see LICENSE for further details
+
+import mdb
+import message
+import pwd
+
+################################################################################
+
+class index :
+  # stolen from python website
+  # and mashed around a bit
+  class groupby:
+    def __init__(self, iterable, keyfunc=None, keyfact=None, grpfact=None):
+      self.it = iter(iterable)
+      self.tgtkey = self.currkey = self.currvalue = object()
+      self.keyfunc, self.keyfact, self.grpfact = keyfunc, keyfact, grpfact
+    def __iter__(self):
+      return self
+    def __next__(self):
+      while self.currkey == self.tgtkey:
+        self.currvalue = next(self.it)    # Exit on StopIteration
+        self.currkey = self.keyfunc(self.currvalue)
+      self.tgtkey = self.currkey
+      return self.keyfact(self)
+ 
+    next=__next__
+    def _grouper(self, tgtkey):
+      while self.currkey == tgtkey:
+        yield self.grpfact(self)
+        self.currvalue = next(self.it)    # Exit on StopIteration
+        self.currkey = self.keyfunc(self.currvalue)
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  class log(dict) :
+
+    class messages(list) :
+      class message(dict) :
+        def __init__(self, values) :
+          dict.__init__(self, values)
+        def __getattr__(self, attr) :
+          return self[attr]
+        @property
+        def isfail(self) :
+          return self.level >= message.ERROR
+      def __init__(self, msgs) :
+        list.__init__(self, map(self.message, msgs))
+        self.sort(key=lambda k : k.level, reverse=True)
+      def __getattr__(self, attr) :
+        attr = attr.upper()
+        for msg in self :
+          if msg.severity == attr : return msg
+        return None
+
+    def __init__(self, log, msgs, **kwargs) :
+      dict.__init__(self, log = log, msgs = self.messages(msgs))
+      self.update(status=self.compute_status())
+
+    def __getattr__(self, attr) :
+      try : 
+        return self[attr]
+      except :
+        return self.msgs.__getattr__(attr)
+
+    def compute_status(self) :
+      'compute status'
+      if self.first.isfail :
+        return dict(status='FAIL', reason='('+self.first.severity+') '+self.first.msg)
+      if self.SUCCESS :
+        if self.SUCCESS.count == 1 :
+	  return dict(status='PASS', reason=self.SUCCESS.msg)
+        else :
+          return dict(status='FAIL', reason='Too many SUCCESSes (%d)' % self.SUCCESS.count)
+      return dict(status='FAIL', reason='No SUCCESS')
+
+    @property
+    def first(self) :
+      return self.msgs[0]
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  class limit :
+    def __init__(self, finish=None, start=None) :
+      self.start, self.finish = start, finish
+    def __str__(self) :
+      if self.finish :
+        if self.start :
+          return "LIMIT %(start)d, %(finish)d" % self.__dict__
+        else :
+          return "LIMIT %(finish)d" % self.__dict__
+      return ""
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  order = 'log_id, msg_id, level ASC'
+
+  def result(self, where, limit) :
+    return [self.log(log, msgs) for log, msgs in self.groupby(self.execute(where, limit), lambda x : x.log_id, self.keyfact, self.grpfact)]
+
+  def where(self, variant) :
+    if variant == 'sngl' :
+      return 'SELECT l0.*, null as children FROM log as l0 left join log as l1 on (l0.log_id = l1.root) where l1.log_id is null and l0.root is null'
+    elif variant == 'rgr' :
+      return 'SELECT l0.*, count(l1.log_id) as children FROM log as l0 left join log as l1 on (l0.log_id = l1.root) group by l0.log_id having l1.log_id is not null'
+    else :
+      return 'SELECT l0.*, count(l1.log_id) as children FROM log as l0 left join log as l1 on (l0.log_id = l1.root) group by l0.log_id'
+
+  def execute(self, where, limit) :
+    with mdb.db.connection().row_cursor() as db :
+      db.execute('SELECT log.*, message.*, COUNT(*) AS count FROM (%s ORDER BY log_id DESC %s) AS log NATURAL LEFT JOIN message GROUP BY log_id, level ORDER BY %s;' % (where, str(limit), self.order))
+      return db.fetchall()
+
+  @staticmethod
+  def keyfact(self) :
+    'key factory for grouping'
+    return [dict(user=pwd.getpwuid(self.currvalue.uid).pw_name, **self.currvalue), self._grouper(self.tgtkey)]
+  @staticmethod
+  def grpfact(self) :
+    'group factory for grouping'
+    return dict(level=self.currvalue.level, severity=self.currvalue.severity, msg=self.currvalue.msg, count=self.currvalue.count)
+
+################################################################################
+
+class msgs :
+  def result(self, log_id):
+    with mdb.db.connection().row_cursor() as db :
+      message.debug('retrieving %(log_id)s messages', log_id=log_id)
+      db.execute('SELECT * FROM message WHERE log_id = %(log_id)s;' % locals())
+      return db.fetchall()
+
+################################################################################
+
+class rgr(index) :
+  order = 'parent ASC, log_id, msg_id, level ASC'
+  
+  def result(self, log_id):
+    return index.result(self, 'SELECT l0.*, count(l1.log_id) as children FROM log as l0 left join log as l1 on (l0.log_id = l1.root) WHERE l0.log_id = %(log_id)s or l0.root = %(log_id)s group by l0.log_id' % locals(), self.limit())
+
+################################################################################
--- www/report.py
+++ www/report.py
@@ -2,12 +2,11 @@
 
 import bottle
 import cStringIO
-import itertools
+import database
 import json
 import mdb
 import message
 import os.path
-import pwd
 import sys
 import time
 
@@ -46,6 +45,7 @@ static = os.path.join(options.root, 'static')
 class serve_something(object) :
   CONTENTTYPE='text/html'
   encapsulate=True
+
   def __init__(self) :
     self.page = cStringIO.StringIO()
 
@@ -82,142 +82,30 @@ class serve_something(object) :
 
 ################################################################################
 
-class index(serve_something) :
-  # stolen from python website
-  # and mashed around a bit
-  class groupby:
-    def __init__(self, iterable, keyfunc=None, keyfact=None, grpfact=None):
-      self.it = iter(iterable)
-      self.tgtkey = self.currkey = self.currvalue = object()
-      self.keyfunc, self.keyfact, self.grpfact = keyfunc, keyfact, grpfact
-    def __iter__(self):
-      return self
-    def __next__(self):
-      while self.currkey == self.tgtkey:
-        self.currvalue = next(self.it)    # Exit on StopIteration
-        self.currkey = self.keyfunc(self.currvalue)
-      self.tgtkey = self.currkey
-      return self.keyfact(self)
- 
-    next=__next__
-    def _grouper(self, tgtkey):
-      while self.currkey == tgtkey:
-        yield self.grpfact(self)
-        self.currvalue = next(self.it)    # Exit on StopIteration
-        self.currkey = self.keyfunc(self.currvalue)
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  class result(dict) :
-
-    class messages(list) :
-      class message(dict) :
-        def __init__(self, values) :
-          dict.__init__(self, values)
-        def __getattr__(self, attr) :
-          return self[attr]
-        @property
-        def isfail(self) :
-          return self.level >= message.ERROR
-      def __init__(self, msgs) :
-        list.__init__(self, map(self.message, msgs))
-        self.sort(key=lambda k : k.level, reverse=True)
-      def __getattr__(self, attr) :
-        attr = attr.upper()
-        for msg in self :
-          if msg.severity == attr : return msg
-        return None
-
-    def __init__(self, log, msgs, **kwargs) :
-      dict.__init__(self, log = log, msgs = self.messages(msgs))
-      self.update(status=self.compute_status())
-
-    def __getattr__(self, attr) :
-      try : 
-        return self[attr]
-      except :
-        return self.msgs.__getattr__(attr)
-
-    def compute_status(self) :
-      'compute status'
-      if self.first.isfail :
-        return dict(status='FAIL', reason='('+self.first.severity+') '+self.first.msg)
-      if self.SUCCESS :
-        if self.SUCCESS.count == 1 :
-	  return dict(status='PASS', reason=self.SUCCESS.msg)
-        else :
-          return dict(status='FAIL', reason='Too many SUCCESSes (%d)' % self.SUCCESS.count)
-      return dict(status='FAIL', reason='No SUCCESS')
-
-    @property
-    def first(self) :
-      return self.msgs[0]
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  class limit :
-    def __init__(self, finish=None, start=None) :
-      self.start, self.finish = start, finish
-    def __str__(self) :
-      if self.finish :
-        if self.start :
-          return "LIMIT %(start)d, %(finish)d" % self.__dict__
-        else :
-          return "LIMIT %(finish)d" % self.__dict__
-      return ""
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+class index(serve_something, database.index) :
   CONTENTTYPE='application/json'
   encapsulate=False
-  order = 'log_id, msg_id, level ASC'
+
   def doit(self, variant, start=0, finish=20):
-    self.json(self.where(variant), self.limit(finish, start))
-
-  def json(self, where, limit) :
-    json.dump([self.result(log, msgs) for log, msgs in self.groupby(self.execute(where, limit), lambda x : x.log_id, self.keyfact, self.grpfact)], self.page)
-
-  def where(self, variant) :
-    if variant == 'sngl' :
-      return 'SELECT l0.*, null as children FROM log as l0 left join log as l1 on (l0.log_id = l1.root) where l1.log_id is null and l0.root is null'
-    elif variant == 'rgr' :
-      return 'SELECT l0.*, count(l1.log_id) as children FROM log as l0 left join log as l1 on (l0.log_id = l1.root) group by l0.log_id having l1.log_id is not null'
-    else :
-      return 'SELECT l0.*, count(l1.log_id) as children FROM log as l0 left join log as l1 on (l0.log_id = l1.root) group by l0.log_id'
-
-  def execute(self, where, limit) :
-    with mdb.db.connection().row_cursor() as db :
-      db.execute('SELECT log.*, message.*, COUNT(*) AS count FROM (%s ORDER BY log_id DESC %s) AS log NATURAL LEFT JOIN message GROUP BY log_id, level ORDER BY %s;' % (where, str(limit), self.order))
-      return db.fetchall()
-
-  @staticmethod
-  def keyfact(self) :
-    'key factory for grouping'
-    return [dict(user=pwd.getpwuid(self.currvalue.uid).pw_name, **self.currvalue), self._grouper(self.tgtkey)]
-  @staticmethod
-  def grpfact(self) :
-    'group factory for grouping'
-    return dict(level=self.currvalue.level, severity=self.currvalue.severity, msg=self.currvalue.msg, count=self.currvalue.count)
+    json.dump(self.result(self.where(variant), self.limit(finish, start)), self.page)
 
 ################################################################################
 
-class msgs(serve_something) :
+class msgs(serve_something, database.msgs) :
   CONTENTTYPE='application/json'
   encapsulate=False
+
   def doit(self, log_id):
-    db = mdb.db.connection().row_cursor()
-    message.debug('retrieving %(log_id)s messages', log_id=log_id)
-    db.execute('SELECT * FROM message WHERE log_id = %(log_id)s;' % locals())
-    json.dump(db.fetchall(), self.page)
-    return
+    json.dump(self.result(log_id), self.page)
 
 ################################################################################
 
-class rgr(index) :
-  order = 'parent ASC, log_id, msg_id, level ASC'
-  
+class rgr(serve_something, database.rgr) :
+  CONTENTTYPE='application/json'
+  encapsulate=False
+
   def doit(self, log_id):
-    self.json('SELECT l0.*, count(l1.log_id) as children FROM log as l0 left join log as l1 on (l0.log_id = l1.root) WHERE l0.log_id = %(log_id)s or l0.root = %(log_id)s group by l0.log_id' % locals(), self.limit())
+    json.dump(self.result(log_id), self.page)
 
 ################################################################################
 
@@ -242,7 +130,6 @@ for path, cls in urls:
       return _cls().GET(**args)
     return fn
   bottle.route(path, name='route_'+cls.__name__)(serve(cls))
-
 
 ################################################################################
 
