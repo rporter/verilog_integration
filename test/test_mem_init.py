22f5cce2148206dc4308a5015410c16b93706062
Rich Porter <dung_spreader@yahoo.com>
2013-06-03 14:34:55 +0100

    faff

--- test/test_mem_init.py
+++ test/test_mem_init.py
@@ -17,13 +17,16 @@ class thistest(test.test) :
     self.mdb.filter_fn = lambda cb_id, level, filename : level < message.INFORMATION
     
     duv.mem[0] = 69
+    duv.mem[69] = 666
+    totin = 0
     message.note('begin initialize')
     for idx, r in enumerate(duv.mem) :
        r.set_value(verilog.vpiInt(idx))
-    message.note('end initialize')
+       totin += idx
+    message.note('end initialize at %(idx)d, sum is %(tot)d', idx=idx, tot=totin)
     message.note('begin read')
-    sum = reduce(lambda a,b : int(a)+int(b), duv.mem)
-    message.note('end read, sum is %(sum)d', sum=sum)
+    totout = reduce(lambda a,b : int(a)+int(b), duv.mem)
+    message.note('end read, sum is %(tot)d', tot=totout)
   def epilogue(self) :
     message.success('memory[0] is %d' % int(duv.mem[0]))
 
