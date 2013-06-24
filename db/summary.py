88506848132dd30a2dd7acf53b08f299c14f7230
Rich Porter <dung.spreader@gmail.com>
2013-06-24 20:25:09 +0100

    always print results

--- db/summary.py
+++ db/summary.py
@@ -15,19 +15,15 @@ options, values = parser.parse_args()
 
 mdb.db.connection.set_default_db(db=os.path.join(options.root, '../db/mdb.db'))
 message.message.summary(False)
-#def cb(*args) : print 'cb'
-#message.terminate_cbs.add('exit', 99, cb, cb) 
-
 
 for log_id in map(int, values) :
   results = database.rgr().result(log_id, options.regression)
-  result = results.summary()
-  if options.regression :
-    for test in results :
-      (message.warning if test.status.status != 'PASS' else message.note)("[%(log_id)d, %(status)s] %(reason)s", log_id=test.log.log_id, **test.status)
+  result = results.summary(True)
+  for test in results :
+    (message.warning if test.status.status != 'PASS' else message.note)("[%(log_id)d, %(status)s] %(reason)s", log_id=test.log.log_id, **test.status)
   if result.passes != result.total :
     msg = message.error
   else :
     msg = message.information
-  msg('%(total)d tests, %(passes)d pass, %(fails)d fail', **result)
+  msg('%(total)d %(tests)s, %(passes)d pass, %(fails)d fail', tests='test' if result.total == 1 else 'tests', **result)
 
