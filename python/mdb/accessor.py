0917c01609a0c8ad05f5a4d6c7c585b0a6c9dbd2
Rich Porter <dung.spreader@gmail.com>
2013-10-24 12:27:29 +0100

    hide internals on iter

--- python/mdb/accessor.py
+++ python/mdb/accessor.py
@@ -5,26 +5,33 @@ def as_a_accessor(cursor) :
     yield accessor(**row)
 
 class accessor(dict) :
+  
   def __init__(self, init=[], **kwargs):
     if init : kwargs.update(dict(init))
     if '_init' in kwargs : del kwargs['_init']
     dict.__init__(self, **kwargs)
     self._init = True
-
+  
   def __getattr__(self, attr) :
     try :
       return self[attr]
     except :
       return None
-
+  
   def __setattr__(self, attr, val) :
     if hasattr(self, '_init') :
       self[attr] = val
     else :
       setattr(self, attr, val)
-
+  
   def __add__(self, other) :
     return self.__class__(self.items() + other.items())
+  
+  def __iter__(self) :
+    'iterator which omits internal keys'
+    for key in self.iterkeys() :
+      if not key.startswith('_') :
+        yield key
 
 def accessor_factory(cursor, row) :
   'Horrible, horrible hack here. Reverse list as when there are duplicates for fields we want use to 1st'
