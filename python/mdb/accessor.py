e9000488d0d3549b9a167b193a3b0bc0031910a3
Rich Porter <dung.spreader@gmail.com>
2013-10-24 13:32:54 +0100

    not interested in some attributes when set

--- python/mdb/accessor.py
+++ python/mdb/accessor.py
@@ -19,20 +19,14 @@ class accessor(dict) :
       return None
   
   def __setattr__(self, attr, val) :
-    if hasattr(self, '_init') :
+    if hasattr(self, '_init') and not attr.startswith('_') :
       self[attr] = val
     else :
-      setattr(self, attr, val)
+      dict.__setattr__(self, attr, val)
   
   def __add__(self, other) :
     return self.__class__(self.items() + other.items())
   
-  def __iter__(self) :
-    'iterator which omits internal keys'
-    for key in self.iterkeys() :
-      if not key.startswith('_') :
-        yield key
-
 def accessor_factory(cursor, row) :
   'Horrible, horrible hack here. Reverse list as when there are duplicates for fields we want use to 1st'
   return accessor(reversed([(name[0], row[idx]) for idx, name in enumerate(cursor.description)]))
